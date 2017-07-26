a65dc7943a1a19f84154c636df51d4213e94c5e0
Garett Bass <garett.bass@king.com>
2017-07-26 13:34:43 -0700

    removed #include <dlfcn.h> from library.hpp

--- detail/library.hpp
+++ detail/library.hpp
@@ -1,48 +1,69 @@
 #pragma once
-#include <dlfcn.h>
+#include <cstdio>
 
 namespace OBJC_NAMESPACE {
+
+    extern "C" {
+
+        enum : int { RTLD_LAZY = 0x1, RTLD_LOCAL = 0x4 };
+
+        void* dlopen(const char*,int);
+
+        void dlclose(void*);
+
+        const char* dlerror(void);
+
+        void* dlsym(void*,const char*);
+
+    } // extern "C"
 
     struct library final {
 
         struct symbol_t {
-            void* const pointer = nullptr;
+            void* const address = nullptr;
 
-            symbol_t(void* pointer) : pointer(pointer) {}
+            symbol_t(void* address) : address(address) {}
 
-            explicit operator bool() const { return pointer != nullptr; }
+            explicit operator bool() const { return address != nullptr; }
 
-            template<typename T> operator T*() const { return (T*)pointer; }
+            template<typename T> operator T*() const { return (T*)address; }
 
-            symbol_t operator *() const { return {*(void**)pointer}; }
+            symbol_t operator *() const { return {*(void**)address}; }
         };
 
-        void* const pointer = nullptr;
+        template<typename>
+        struct function_t;
+
+        template<typename Result, typename... Args>
+        struct function_t<Result(Args...)> {
+            using pointer = Result(*)(Args...);
+        };
+
+        void* const address = nullptr;
 
         library() = default;
 
         library(decltype(nullptr)) : library() {}
 
-        library(const char* path, int mode = RTLD_NOW | RTLD_LOCAL) :
-            pointer(dlopen(path,mode))
+        library(const char* path, int mode = RTLD_LAZY | RTLD_LOCAL)
+        : address(dlopen(path,mode))
         {
-            if (not pointer) {
-                const char* error = dlerror();
-                printf("library \"%s\" %s\n",path,error?error:"not found");
+            if (not address) {
+                fprintf(stderr,"library \"%s\" not found\n",path);
             }
-            // else { printf("library \"%s\" @ %p\n",path,pointer); }
+            // else { printf("library \"%s\" @ %p\n",path,address); }
         }
 
-        library(library&& rhs) : pointer(rhs.pointer) { new(&rhs) library(); }
+        library(library&& rhs) : address(rhs.address) { new(&rhs) library(); }
 
         library& operator =(library&& rhs) {
             if (this != &rhs) { new(this) library(std::move(rhs)); }
             return *this;
         }
 
-        ~library() { if (pointer) { dlclose(pointer); } }
+        ~library() { if (address) { dlclose(address); } }
 
-        explicit operator bool() const { return pointer != nullptr; }
+        explicit operator bool() const { return address != nullptr; }
 
         symbol_t operator ()(const char* name) const { return symbol(name); }
 
@@ -51,10 +72,12 @@ namespace OBJC_NAMESPACE {
         symbol_t function(const char* name) const { return symbol(name); }
 
         symbol_t symbol(const char* name) const {
-            symbol_t sym { pointer ? dlsym(pointer,name) : nullptr };
-            if (not sym) { printf("symbol \"%s\" not found\n", name); }
-            // else { printf("symbol \"%s\" @ %p\n",name,sym.pointer); }
-            return sym;
+            symbol_t symbol { address ? dlsym(address,name) : nullptr };
+            if (not symbol) {
+                fprintf(stderr,"symbol \"%s\" not found\n",name);
+            }
+            // else { printf("symbol \"%s\" @ %p\n",name,sym.address); }
+            return symbol;
         }
 
     };
